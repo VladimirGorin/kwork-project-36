a9bf644acc014203359078e91d15ec265b5c1662
VladimirGorin <vladimirgorin410@gmail.com>
2024-01-29 16:05:57 +0300

    first

--- assets/settings/settings.js
+++ assets/settings/settings.js
@@ -1,7 +1,7 @@
 module.exports.oauth_token = "6da66d21d07347678e947ee679d5bda2"
 module.exports.counterId = 92059143
 module.exports.token_test = "6582380572:AAFB-IS1uTcosUz2T-8R18hfRrgvyeOku7A"
-module.exports.token = "6864688989:AAGXqUZh-PqUeJoNZG6ddRVObCxcu12lTak"
+module.exports.token = "6971614163:AAGbhu84b9vjZFVe2hftoNYPEWKQiVQ9XQU"
 module.exports.VONAGE_API_KEY = "4fa85503"
 module.exports.VONAGE_API_SECRET  = "5wmd4tLeXGHWcRuL"
 module.exports.port = 8000
